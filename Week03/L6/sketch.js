946a0f6b9f968bbbd0b227d17d5d0ccc8b088a83
Prahlad G Menon, PhD <menon.prahlad@gmail.com>
2021-07-23 17:13:40 -0400

    Update model path
    https://teachablemachine.withgoogle.com/models/NSx2zxPZN/

--- Week03/L6/sketch.js
+++ Week03/L6/sketch.js
@@ -12,7 +12,7 @@ This example uses p5 preload function to create the classifier
 // Classifier Variable
 let classifier;
 // Model URL
-let imageModelURL = 'https://teachablemachine.withgoogle.com/models/bXy2kDNi/';
+let imageModelURL = 'https://teachablemachine.withgoogle.com/models/NSx2zxPZN/';
 
 // Video
 let video;
